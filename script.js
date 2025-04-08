197a0777cfa00a9e248dc998234186b6f6704ef3
carolinepereiraana <ana.silva.pereira10@ESCOLA.PR.GOV.BR>
2025-04-08 09:46:58 -0300

    ok

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 });
 
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+   
