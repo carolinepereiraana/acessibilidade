d13ecad6149a71e102ad35403806fd39c4a83a99
carolinepereiraana <ana.silva.pereira10@ESCOLA.PR.GOV.BR>
2025-04-22 08:56:34 -0300

    .

--- script.js
+++ script.js
@@ -26,4 +26,5 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
-   
+   const alternaContraste = document.getElementById('alterna-contraste')
+
